518d6465a33fa0430747feb0d32be8a7d1f214ea
wun1up1302 <hardys2100@student.faytechcc.edu>
2025-09-21 12:37:21 -0400

    Add files via upload

---
+++ P1LAB1_StevieHardy.py
@@ -0,0 +1,8 @@
+# Stevie Hardy
+# 09/20/2025
+# P1LAB1
+# User enters frist and last name and "Welcome to CTI-110"
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,", first_name, last_name + "!", "welcome to CTI-110!")
+
